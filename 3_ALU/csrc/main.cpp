146b517a49ee1f942666cd9281fdbc8910db16c8
Sin-kider <2210486110@qq.com>
2023-01-25 18:10:51 +0800

    完成四位加法器

--- 3_ALU/csrc/main.cpp
+++ 3_ALU/csrc/main.cpp
@@ -1,3 +1,4 @@
+#include "iostream"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 #include "Vtop.h"
@@ -30,11 +31,68 @@ void sim_exit()
     tfp->close();
 }
 
-int main() 
-{ 
+void print_4bin(unsigned char binnum)
+{
+    for (unsigned char i = 0; i < 4; i++)
+    {
+        std::cout << (!(!(binnum & (0b1000 >> i))));
+    }
+}
+
+int two2sign(unsigned char twonum)
+{
+    int signnum = 0;
+    if (!(twonum & 0b00001000))
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            signnum += (!(!(twonum & (1 << i)))) * (1 << i);
+        }
+    }
+    else
+    {
+        if ((twonum & 0x0F) == 0b1000)
+        {
+            return -8;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            signnum -= (!(!(~(twonum - 1) & (1 << i)))) * (1 << i);
+        }
+    }
+    return signnum;
+}
+
+unsigned char sign2two(int signnum) 
+{
+
+    unsigned char twonum = 0b1000;
+    if (signnum >= 0)
+    {
+        twonum = signnum;
+        return twonum;
+    }
+    else
+    {
+        twonum = (signnum & 0b00000111) | 0b00001000;
+        return twonum;
+    }
+}
+
+int main()
+{
     sim_init();
 //
-
+    top->a      = sign2two(-1);
+    top->b      = sign2two(-3);
+    top->cin    = sign2two(0);
+    step_and_dump_wave();
+    std::cout << "a = " << two2sign(int(top->a)) << " "; print_4bin(top->a); std::cout << std::endl;
+    std::cout << "b = " << two2sign(int(top->b)) << " "; print_4bin(top->b); std::cout << std::endl;
+    std::cout << "cin = " << two2sign(int(top->cin)) << " "; print_4bin(top->cin); std::cout << std::endl;
+    std::cout << "sum = " << two2sign(int(top->sum)) << " "; print_4bin(top->sum); std::cout << std::endl;
+    // std::cout << "cout = " << two2sign(int(top->cout)) << " "; print_4bin(top->cout); std::cout << std::endl;
+    std::cout << std::endl;
 //
     sim_exit();
 }
